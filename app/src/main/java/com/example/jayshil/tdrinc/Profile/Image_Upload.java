22c8c2e5dc3316b3147ccd71da8c8d4cbaa73e1e
jayshil12 <jayshil72@gmail.com>
2018-04-14 23:58:44 -0700

    First commit

---
+++ app/src/main/java/com/example/jayshil/tdrinc/Profile/Image_Upload.java
@@ -0,0 +1,22 @@
+package com.example.jayshil.tdrinc.Profile;
+
+public class Image_Upload {
+
+    public String imageUri;
+
+    public Image_Upload(){
+
+    }
+
+    public Image_Upload(String imageUri){
+        this.imageUri = imageUri;
+    }
+
+    public void setImageUri(String imageUri) {
+        this.imageUri = imageUri;
+    }
+
+    public String getImageUri() {
+        return imageUri;
+    }
+}
